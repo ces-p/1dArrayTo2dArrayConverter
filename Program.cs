cfb89320d5d75f1a9c2c96e124338424863e7fec
ces-p <cesarportillord@gmail.com>
2021-05-28 19:25:29 -0400

    First commit 1d Arrays to 2d arrays converter

--- Program.cs
+++ Program.cs
@@ -5,34 +5,35 @@ namespace _2d_arrays
     class Program
     {
         static void Main(string[] args){
-            int[] Table = declareTable(2,3);
-            foreach( var item in Table){
-                Console.WriteLine(item);
-            }
-            Console.WriteLine(readTableCell(Table, 1,1));
-            Console.WriteLine(writeTableCell(Table, 1,1, 1));
-            foreach(var item in Table){
-                Console.WriteLine(item);
-            }
+           int[] table = declareTable(2,3);
+           foreach(var item in table){
+               Console.WriteLine(item);
+           }
+            Console.WriteLine(readTableCell(table,0,0));  
+            Console.WriteLine(writeTableCell(table,0,0,30)); 
+            foreach(var item in table){
+               Console.WriteLine(item);
+           } 
         }
     //declareTable(rows: int, columns: int) : int[]
         static int[] declareTable(int rows, int columns){
             int s = rows*columns;
             int[] Table = new int[s];
-            for(int i = 0; i < s; i++){
-                Table[i] = i;
+
+            for(int i = 0; i<s; i++){
+                Table[i] = i *3;
             }
             return Table;
         }
     //readTableCell(table: int[], row : int, column: int) : int
         static int readTableCell(int[] table, int row, int column){
-            return table[(2*row)+column];
+            return table[row*row + column];
         }
     //writeTableCell(table: int[], row: int, column: int) : int //old-value
         static int writeTableCell(int[] table, int row, int column, int newValue){
-            int oldValue = table[(2*row)+column];
-            table[(2*row)+column] = newValue;
+             int oldValue = table[row*row + column];
+             table[row*row + column] = newValue;
             return oldValue;
-        }    
+        }
     }
 }
